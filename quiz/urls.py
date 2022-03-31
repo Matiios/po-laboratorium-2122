5ba6766b72de2746639461db6ea5dd47d9499b5f
Matiios <matiius11@gmail.com>
2022-03-31 21:16:32 +0200

    changed views methods. add exception handle to the vote function. Created results.html and changed other html files

--- quiz/urls.py
+++ quiz/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
 
+
+app_name = 'quiz'
 urlpatterns = [
-    path ('', views.check_func, name = 'TEST'),
-    path('<int:question_id>/', views.detail, name='detail'),
+    path ('', views.check_func, name = 'index'),
+    path('specifics/<int:question_id>/', views.detail, name='detail'),
     path('<int:question_id/results>/', views.results, name='results'),
     path('<int:question_id/vote>/', views.vote, name='vote'),
 
